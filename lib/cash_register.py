e07a96e5c13b89ed533e5bdf2470bd8ac60fe18b
AnitaNamjoon <135642350+AnitaNamjoon@users.noreply.github.com>
2023-08-26 12:30:17 +0300

    Update cash_register.py

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.items = []
+        self.total = 0
+        self.last_transaction_amount = 0
+        self.discount = discount
+
+    def add_item(self, item_name, price, quantity=1):
+        item_cost = price * quantity
+        self.total += item_cost
+        self.last_transaction_amount = item_cost
+        self.items.append({"item": item_name, "price": price, "quantity": quantity})
+
+    def apply_discount(self):
+        if self.discount >= 0 and self.discount <= 100:
+            discount_factor = 1 - self.discount / 100
+            self.total *= discount_factor
+            return self.total
+        else:
+            return "Invalid discount percentage"
+
+    def void_last_transaction(self):
+        if self.items:
+            self.total -= self.last_transaction_amount
+            self.items.pop()
+            self.last_transaction_amount = 0
+        else:
+            return "No transactions to void"
